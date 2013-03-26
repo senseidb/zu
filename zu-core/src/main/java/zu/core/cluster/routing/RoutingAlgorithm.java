0385894e3badc8a3f95fe4dca39cd45ed11c681a
John Wang <john.wang@gmail.com>
2013-03-26 00:29:41 -0700

    removed warning

--- zu-core/src/main/java/zu/core/cluster/routing/RoutingAlgorithm.java
+++ zu-core/src/main/java/zu/core/cluster/routing/RoutingAlgorithm.java
@@ -2,7 +2,6 @@ package zu.core.cluster.routing;
 
 import java.net.InetSocketAddress;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
